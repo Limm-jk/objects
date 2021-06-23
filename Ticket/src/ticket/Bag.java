867f44e315cb71a9101b6c0e669e8c3fa3065b03
limm-jk <junkyu.lim.dev@gmail.com>
2021-06-24 01:14:46 +0900

    add : bag constructor

--- Ticket/src/ticket/Bag.java
+++ Ticket/src/ticket/Bag.java
@@ -5,6 +5,16 @@ public class Bag {
     private Invitation invitation;
     private Ticket ticket;
 
+    // 생성 시에 제약을 강제
+    public Bag(long amount) {
+        this(null, amount);
+    }
+
+    public Bag(Invitation invitation, long amount) {
+        this.invitation = invitation;
+        this.amount = amount;
+    }
+
     public boolean hasInvitation() {
         return invitation != null;
     }
